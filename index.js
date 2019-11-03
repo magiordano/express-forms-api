1b6d074d31921815b1b926f5d158002e4cdb4af3
Mike <magiordano77@gmail.com>
2019-11-03 16:49:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 var cors = require('cors')
-//require('dotenv').config();
+require('dotenv').config();
 const app = express()
 const port = 5000
 
